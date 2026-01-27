3dfb6de57d68266ec04e6e618a228188c4f73e4a
Vinicius <vinicius.2010.s@gmail.com>
2026-01-27 14:28:44 -0300

    fix(backend): fix sqlite migration syntax and bump version to 1.0.7

--- backend/migrations/20260108_01_NkDqU-add-penalty-check-to-dungeons-entries.sql
+++ backend/migrations/20260108_01_NkDqU-add-penalty-check-to-dungeons-entries.sql
@@ -2,5 +2,6 @@
 -- depends: 20251214_01_HBiJB-create-character-schedules-table  20251229_01_add-character-details  20260107_01_7ZtiU-add-inventory
 
 ALTER TABLE dungeons_entries
-ADD COLUMN has_penalty BOOLEAN NOT NULL DEFAULT FALSE,
-ADD COLUMN floor INTEGER;
+ADD COLUMN has_penalty BOOLEAN NOT NULL DEFAULT FALSE;
+
+ALTER TABLE dungeons_entries ADD COLUMN floor INTEGER;
